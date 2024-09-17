b3e579b40dd62d5e63ab292795c751084a6eb710
Esuyawkal <esuyawkal07@gmail.com>
2024-09-17 18:35:24 +0300

    first commit

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ document.addEventListener('DOMContentLoaded',()=>{
     const array = Array.from(icons);
     const counter = document.getElementsByClassName('counter');
     const counters = Array.from(counter);
-    const cont = document.getElementsByClassName('icon-container');
-    const container = Array.from(cont);
+    const prof = document.getElementsByClassName('profile-icon');
+    const profile = Array.from(prof);
+
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 256); // Red value between 0 and 255
+        const g = Math.floor(Math.random() * 256); // Green value between 0 and 255
+        const b = Math.floor(Math.random() * 256); // Blue value between 0 and 255
+        return `rgb(${r}, ${g}, ${b})`;}
+    
+profile.forEach((element) => {
+    element.style.backgroundColor=getRandomColor();
+})
 
     array.forEach((element,index) => {
         element.addEventListener('click',()=>{
